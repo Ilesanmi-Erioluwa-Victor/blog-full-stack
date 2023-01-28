dd2add956493c67592fbd3726bc4e72e072ba671
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-28 14:12:41 +0100

    Working on Signup

--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 
 const Signup = ():JSX.Element => {
     const [passwordFieldType, setPasswordFieldType] = useState<boolean>(false);
+
+      const togglePasswordVisibility = () => {
+    setPasswordFieldType(!passwordFieldType);
+  };
+  
   return (
     <div>
        <h2>Hello from sign up</h2>
