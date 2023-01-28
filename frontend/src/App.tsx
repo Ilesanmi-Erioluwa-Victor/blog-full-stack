4449572fffbaedb07d7f3acd57eb16b283b01d61
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-28 14:09:10 +0100

    Working on Signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom"
-import Error from "./pages/Error";
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { Error, Home, Signup, Login } from "./pages";
 
 function App() {
   return (
     <Routes>
       <Route element={<Home />} path="/" />
-      <Route  element={<Error />} path="*"/>
-      
+      <Route element={<Error />} path="*" />
+      <Route element={<Signup />} path="/signup" />
+      <Route element={<Login />} path="/login" />
     </Routes>
   );
 }
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home"
+export { default as Error } from "./Error"
+export { default as Signup } from "./auth/Signup"
+export { default as Login } from "./auth/Login"
