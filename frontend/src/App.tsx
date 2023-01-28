52ac4bc65e530f9e6a304a614a47d0ff1ae055d7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-28 10:22:03 +0100

    Working on Search atom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from './components/atoms/Button/Button';
 import NetworkStatus from './components/atoms/NetworkStatus/NetworkStatus';
+import Search from './components/atoms/Search/Search';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <h1 className="text-3xl font-bold underline text-orange-700">
     Hello world!
   </h1>
-
+<Search placeholder='Search your content' className={"w-[50%]"} inputClass={"pr-2"} childClasses={"focus:border-none"}/>
       </header>
     </div>
     </NetworkStatus>
--- frontend/src/components/atoms/Search/Search.tsx
+++ frontend/src/components/atoms/Search/Search.tsx
@@ -3,23 +3,25 @@ import PropTypes, { InferProps } from "prop-types";
 
 const SearchProps = {
   placeholder: PropTypes.string.isRequired,
-  classes: PropTypes.string,
+  className: PropTypes.string,
   childClasses: PropTypes.string,
+  inputClass: PropTypes.string,
 };
 export default function Search({
   placeholder,
-  classes,
+  className,
   childClasses,
+  inputClass
 }: InferProps<typeof SearchProps>): JSX.Element {
   return (
-    <div className={`w-full ${classes}`}>
+    <div className={`${className}`}>
       <div
         className={`w-full flex items-center space-x-3 border bg-white h-12 pl-3 rounded-lg ${childClasses}`}>
         {/* <MagnifyingGlassIcon className="w-5 h-5" /> */}
         <input
           type="text"
           placeholder={placeholder}
-          className="focus:outline-none w-full pr-8"
+          className={`focus:outline-none w-full pr-8 ${inputClass}`}
         />
       </div>
     </div>
