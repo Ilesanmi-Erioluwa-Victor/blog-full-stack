39e21fa7a5dcc75ed3d7965f295a146704807ede
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-28 15:35:27 +0100

    Working on Signup

--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 const Signup = ():JSX.Element => {
@@ -12,9 +13,19 @@ const Signup = ():JSX.Element => {
     <div className="flex padding bg-orange-300 w[100%] relative">
        {/* First Section */}
        <section className="flex sec-flex">
-          <nav>
-            
+          <nav className="sec-flex">
+            <Link to={"/"}>
+                <h3 className="text-xl text-orange-700 font-bold mb-[7rem]">Blog Platform</h3>
+            </Link>
+            <h2 className="text-2xl">Join Our blogging platform, to experience unlimited Resources.</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam aperiam unde nobis excepturi sed. Magnam?</p>
           </nav>
+       </section>
+
+       {/* Second Section */}
+       <section>
+        <h1>Hello from Prime</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quas eligendi repellat, perspiciatis est maxime, libero dolorum molestias cupiditate ab veritatis sunt tempora sit distinctio doloremque obcaecati, cumque voluptas in.</p>
        </section>
     </div>
   )
