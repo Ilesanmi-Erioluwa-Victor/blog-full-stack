0cc5fd306b05f28a3daec9d4a0c240d44d88cc81
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-28 11:46:17 +0100

    Working on Routes, route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import Button from './components/atoms/Button/Button';
-import NetworkStatus from './components/atoms/NetworkStatus/NetworkStatus';
-import Search from './components/atoms/Search/Search';
+import React from "react";
+import { Routes, Route} from "react-router-dom"
+import { Button, Search } from "./components/atoms";
 
 function App() {
   return (
-    <>
-    <NetworkStatus>
-    <div className="App">
-      <header className="App-header">
-       <Button classes={`bg-black p-3 outline-none text-white rounded`} type={"button"}> Button</Button>
+    <Routes>
+      <Route  element={<div>Hello world</div>} path="/"/>
+      <div className="App">
+
+        <header className="App-header">
+          <Button
+            classes={`bg-black p-3 outline-none text-white rounded`}
+            type={"button"}
+          >
+            {" "}
+            Button
+          </Button>
 
           <h1 className="text-3xl font-bold underline text-orange-700">
-    Hello world!
-  </h1>
-<Search placeholder='Search your content' className={"w-[50%]"} inputClass={"pr-2"} childClasses={"focus:border-none"}/>
-      </header>
-    </div>
-    </NetworkStatus>
-    </>
+            Hello world!
+          </h1>
+          <Search
+            placeholder="Search your content"
+            className={"w-[50%]"}
+            inputClass={"pr-2"}
+            childClasses={"focus:border-none"}
+          />
+        </header>
+      </div>
+    </Routes>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-// import { BrowserRouter } from "react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { NetworkStatus } from "./components/atoms";
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NetworkStatus>
+        <App />
+      </NetworkStatus>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
