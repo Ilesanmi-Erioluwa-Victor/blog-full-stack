f9caec6bc452d3f27d7bc3ebd3892d3441f8cab2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-29 04:42:52 +0100

    First commit

--- frontend/src/components/atoms/Input/Input.tsx
+++ frontend/src/components/atoms/Input/Input.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface InputProps {
   placeholder: string;
-  className: string;
+  className?: string;
   label: string | undefined;
   type : string;
   name? : string | undefined;
@@ -14,7 +14,7 @@ interface InputProps {
 const Input: React.FC<InputProps> = ({ placeholder, className, label, type, name, fieldsetClass, onChange, value }) => {
   return (
     <fieldset className={fieldsetClass}>
-      {label && <label htmlFor="name">{label}</label>}
+      {label && <label htmlFor={name}>{label}</label>}
 
       <input
         type={type}
@@ -22,7 +22,7 @@ const Input: React.FC<InputProps> = ({ placeholder, className, label, type, name
         placeholder={placeholder}
         onChange={onChange}
         value={value}
-        className={`placeholder:text-sm text-sm border-gray-400 outline:none focus:border-none border w-full h-10 rounded-md px-4 pr-16 ${className} focus:border-gray-400`}
+        className={`placeholder:text-sm text-sm border-gray-400 outline:none focus:border-none border w-full h-10 rounded-md px-4 pr-16 focus:border-gray-400 ${className}`}
       />
     </fieldset>
   );
--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -1,34 +1,80 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { Input } from "src/components/atoms";
 
-const Signup = ():JSX.Element => {
-    const [passwordFieldType, setPasswordFieldType] = useState<boolean>(false);
+const Signup = (): JSX.Element => {
+  const [passwordFieldType, setPasswordFieldType] = useState<boolean>(false);
 
-      const togglePasswordVisibility = () => {
+  const togglePasswordVisibility = () => {
     setPasswordFieldType(!passwordFieldType);
   };
 
   return (
     <div className="flex padding bg-orange-300 w[100%] relative">
-       {/* First Section */}
-       <section className="flex sec-flex">
-          <nav className="sec-flex">
-            <Link to={"/"}>
-                <h3 className="text-xl text-orange-700 font-bold mb-[7rem]">Blog Platform</h3>
-            </Link>
-            <h2 className="text-2xl">Join Our blogging platform, to experience unlimited Resources.</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam aperiam unde nobis excepturi sed. Magnam?</p>
-          </nav>
-       </section>
+      {/* First Section */}
+      <section className="flex sec-flex w-[70%]">
+        <nav className="sec-flex">
+          <Link to={"/"} className="w-[10rem]">
+            <h3 className="text-xl text-orange-700 font-bold">Blog Platform</h3>
+          </Link>
+          <h2 className="text-[2rem] mt-[5rem]">
+            Join Our blogging platform, to experience unlimited Resources.
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+            aperiam unde nobis excepturi sed. Magnam?
+          </p>
+        </nav>
 
-       {/* Second Section */}
-       <section>
+        {/*  first name and last Name*/}
+        <fieldset className="flex gap-2 w-[100%]">
+          <Input
+            type={"text"}
+            label={"FirstName"}
+            placeholder={"Enter your firstname"}
+            className="mt-2 focus:border-none focus:outline-none transition-all"
+            fieldsetClass="w-[100%]"
+          />
+          <Input
+            type={"text"}
+            label={"LastName"}
+            placeholder={"Enter your lastname"}
+            className="mt-2 focus:outline-none transition-all"
+            fieldsetClass="w-[100%]"
+          />
+        </fieldset>
+              {/*  Emailand last Password*/}
+          <fieldset className="flex gap-2 w-[100%]">
+          <Input
+            type={"email"}
+            label={"Email"}
+            placeholder={"Enter your Email"}
+            className="mt-2 focus:border-none focus:outline-none transition-all"
+            fieldsetClass="w-[100%]"
+          />
+          <Input
+            type={"password"}
+            label={"Password"}
+            placeholder={"Enter your Password"}
+            className="mt-2 focus:outline-none transition-all"
+            fieldsetClass="w-[100%]"
+          />
+        </fieldset>
+        <p>Or</p>
+      </section>
+
+      {/* Second Section */}
+      <section className="w-[30%]">
         <h1>Hello from Prime</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quas eligendi repellat, perspiciatis est maxime, libero dolorum molestias cupiditate ab veritatis sunt tempora sit distinctio doloremque obcaecati, cumque voluptas in.</p>
-       </section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          quas eligendi repellat, perspiciatis est maxime, libero dolorum
+          molestias cupiditate ab veritatis sunt tempora sit distinctio
+          doloremque obcaecati, cumque voluptas in.
+        </p>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
