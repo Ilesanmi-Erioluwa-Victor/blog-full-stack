1ff8143f7272d3475454c8a3d4b27be88ec83b16
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-29 11:08:25 +0100

    Working on Register user thunk

--- frontend/src/redux/Slices/Users/user.tsx
+++ frontend/src/redux/Slices/Users/user.tsx
@@ -1,7 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit" 
+import axios from "axios";
 
 // Register User
 export const userRegisterAction = createAsyncThunk(
     "users/register", async (user, {rejectWithValue, getState, dispatch})=> {
-   
-}); 
+       try {
+        const config = {
+            headers : {
+             "Content-Type" : "application/json",
+            }
+        }
+
+        const response = await axios.post("http://localhost:5000/api/users/register", user, config)
+       } catch (error) {
+        
+       }
+});   
