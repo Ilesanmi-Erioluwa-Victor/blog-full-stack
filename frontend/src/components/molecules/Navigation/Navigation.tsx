cc33f8894d2a1c3cc1bc00055a5026003d8d9ae2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-01 21:19:15 +0100

    Eri, you no try at all

--- frontend/src/components/molecules/Navigation/Navigation.tsx
+++ frontend/src/components/molecules/Navigation/Navigation.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 Admin
+import Admin from "src/pages/Admin/Admin";
 import { useAppSelector } from "src/redux/hooks";
 import { RootState } from "src/redux/store";
 import PrivateNav from "./Private/PrivateNav";
 import PublicNav from "./Public/PublicNav";
-import Admin from "src/pages/Admin/Admin";
 
 const Navigation = (): JSX.Element => {
   const users = useAppSelector((state: RootState) => state.users);
   const { userAuth } = users;
-  // const isAdmin = userAuth?.isAdmin;
+  const isAdmin = userAuth?.isAdmin;
   return (
     <>
       {!userAuth ? (
         <PublicNav />
       ) : userAuth ? (
         <PrivateNav />
-      ) : ""
+      ) : isAdmin && <Admin />
     }
     </>
   );
--- frontend/src/components/molecules/Navigation/Private/PrivateNav.tsx
+++ frontend/src/components/molecules/Navigation/Private/PrivateNav.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { Bars3Icon, PlusIcon} from "@heroicons/react/24/outline";
+import React  from "react";
+import { Bars3Icon, PlusIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import { Button } from "src/components/atoms";
-import GeneralLayout from 'src/layouts/GeneralLayouts/GeneralLayout';
-import { Icon } from 'src/utils';
+import GeneralLayout from "src/layouts/GeneralLayouts/GeneralLayout";
+import { useAppDispatch, useAppSelector } from "src/redux/hooks";
+import { userLogOutAction } from "src/redux/Slices/Users/user";
+import { RootState } from "src/redux/store";
+import { Icon } from "src/utils";
 
 interface headerType {
   title: string;
@@ -34,101 +37,107 @@ let header: headerType[] = [
     link: "/authors",
     active: false,
   },
- 
 ];
 
 const PrivateNav = () => {
- const userNav = [{
-    name : "Your Profile",
-    link : `/profile`
- },
+  const dispatch = useAppDispatch();
+  const users = useAppSelector((state: RootState) => state.users);
 
- {
-    name : "Change your Password",
-    link : `/update-password`
- },
+  // if(!users.userAuth) return <Navigate to={"/login"}/>
 
-]
+  console.log(users);
 
-     const changePath = (path: string) => {
+
+  const userNav = [
+    {
+      name: "Your Profile",
+      link: `/profile`,
+    },
+
+    {
+      name: "Change your Password",
+      link: `/update-password`,
+    },
+  ];
+
+  const changePath = (path: string) => {
     // router.push(path);
   };
 
-    const handleMenuClick = (item: headerType, index: number) => {
+  const handleMenuClick = (item: headerType, index: number) => {
     header.map((item) => (item.active = false));
     // if (router.pathname === item.link) {
     //   header[index].active = true;
     // }
   };
 
-
   return (
     <GeneralLayout>
-   <div className={`w-full h-20 sticky top-0 z-50 bg-white`}>
-      <div className={`relative h-20`}>
-        <header className="w-11/12 relative z-50 md:w-11/12 xl:w-10/12 mx-auto h-full flex justify-between  items-center">
-          <Link to="/">
-            <div className={`w-1/12 h-full flex items-center cursor-pointer`}>
-              <img src="/main_logo.svg" alt="logo" className="" />
-            </div>
-          </Link>
-          <nav className="hidden lg:flex items-center h-full ">
-            <ul className="flex lg:space-x-3 xl:space-x-5">
-              {header.map((item: any, index: number) => (
-                <Link to={item.link} key={index}>
-                  <li
-                    onClick={() => handleMenuClick(item, index)}
-                    className={`text-sm flex capitalize cursor-pointer hover:text-primary_green hover:underline hover:underline-offset-4 ${
-                      item.active &&
-                      "text-primary_green underline underline-offset-4"
-                    }`}
-                  >
-                    <span>{item.title}</span>
-                    <span>{item.icon ? item.icon : ""}</span>
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </nav>
-          <div className="flex space-x-4 items-center">
-            <div className="hidden md:flex space-x-4">
-                <Link to={"/logout"}>
-              <Button className={`w-36 h-10  md:w-28 xl:w-36 rounded-md capitalize text-sm 
-                text-dark_blue border border-dark_blue hover:bg-dark_blue hover:text-white  hover:opacity-80`} >
-                Logout 
-            </Button>
-                </Link>
-
-              <Link to={"/newPost"}>
-              <Button
-                className={`pl-3 w-36 md:w-28 xl:w-36 rounded-md capitalize h-10 text-sm 
-                text-dark_blue border border-dark_blue hover:bg-dark_blue hover:text-white  hover:opacity-80 flex items-center gap-3`}
-              ><PlusIcon className='w-3 self-center'/> New Post </Button>
-              </Link>
-            </div>
-            <div className="flex lg:hidden h-full items-center">
-              <Bars3Icon className="w-8 h-6" />
-            </div>
-
-            <figure className='relative'>
-                <Icon src={""} alt='user' className=''/>
-                <ul className='absolute top-10 right-6 w-full'>
-                {userNav.map( (items : any, index: number) => (
-                    <li key={index} className='w-full bg-red-900'>
-                            <Link to={items.link}>{items.name}</Link>
+      <div className={`w-full h-20 sticky top-0 z-50 bg-white`}>
+        <div className={`relative h-20`}>
+          <header className="w-11/12 relative z-50 md:w-11/12 xl:w-10/12 mx-auto h-full flex justify-between  items-center">
+            <Link to="/">
+              <div className={`w-1/12 h-full flex items-center cursor-pointer`}>
+                <img src="/main_logo.svg" alt="logo" className="" />
+              </div>
+            </Link>
+            <nav className="hidden lg:flex items-center h-full ">
+              <ul className="flex lg:space-x-3 xl:space-x-5">
+                {header.map((item: any, index: number) => (
+                  <Link to={item.link} key={index}>
+                    <li
+                      onClick={() => handleMenuClick(item, index)}
+                      className={`text-sm flex capitalize cursor-pointer hover:text-primary_green hover:underline hover:underline-offset-4 ${
+                        item.active &&
+                        "text-primary_green underline underline-offset-4"
+                      }`}
+                    >
+                      <span>{item.title}</span>
+                      <span>{item.icon ? item.icon : ""}</span>
                     </li>
+                  </Link>
                 ))}
+              </ul>
+            </nav>
+            <div className="flex space-x-4 items-center">
+              <div className="hidden md:flex space-x-4">
+                <Button
+                  className={`w-36 h-10  md:w-28 xl:w-36 rounded-md capitalize text-sm 
+                text-dark_blue border border-dark_blue hover:bg-dark_blue hover:text-white  hover:opacity-80`}
+                  onClick={() => dispatch(userLogOutAction())}
+                >
+                  Logout
+                </Button>
+
+                <Link to={"/newPost"}>
+                  <Button
+                    className={`pl-3 w-36 md:w-28 xl:w-36 rounded-md capitalize h-10 text-sm 
+                text-dark_blue border border-dark_blue hover:bg-dark_blue hover:text-white  hover:opacity-80 flex items-center gap-3`}
+                  >
+                    <PlusIcon className="w-3 self-center" /> New Post{" "}
+                  </Button>
+                </Link>
+              </div>
+              <div className="flex lg:hidden h-full items-center">
+                <Bars3Icon className="w-8 h-6" />
+              </div>
+
+              <figure className="relative">
+                <Icon src={""} alt="user" className="" />
+                <ul className="absolute top-10 right-6 w-full">
+                  {userNav.map((items: any, index: number) => (
+                    <li key={index} className="w-full bg-red-900">
+                      <Link to={items.link}>{items.name}</Link>
+                    </li>
+                  ))}
                 </ul>
-            </figure>
-          </div>
-        </header>
+              </figure>
+            </div>
+          </header>
+        </div>
       </div>
-    </div>
     </GeneralLayout>
-  )
-}
+  );
+};
 
-
-
-
-export default PrivateNav
+export default PrivateNav;
