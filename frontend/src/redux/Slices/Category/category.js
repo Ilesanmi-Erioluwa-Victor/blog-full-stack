14b83cff2e6161bd7beb86bef92c8b0bff0a82ab
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-01 22:45:22 +0100

    Working on createCategory Slice

--- frontend/src/redux/Slices/Category/category.js
+++ frontend/src/redux/Slices/Category/category.js
@@ -1,3 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Baseurl } from "src/utils/Baseurl";
+
+// Action
+
+export const createCategoryAction = createAsyncThunk(
+    "category/create", 
+    async() => {
+        
+    }
+)
