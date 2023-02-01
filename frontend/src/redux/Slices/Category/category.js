7051583676e00266e8cea76b4cbdb5c49b56a0e7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-01 23:43:51 +0100

    working on Authorization of users

--- frontend/src/redux/Slices/Category/category.js
+++ frontend/src/redux/Slices/Category/category.js
@@ -5,51 +5,64 @@ import { Baseurl } from "src/utils/Baseurl";
 // Action
 
 export const createCategoryAction = createAsyncThunk(
-    "category/create", 
-    async(category, {rejectWithValue, getState}) => {
-        // getState, for returning all your state in your request
+  "category/create",
+  async (category, { rejectWithValue, getState }) => {
+    // getState, for returning all your state in your request
+    const user = getState()?.users;
+    const {
+      userAuth: { token },
+    } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
     //   Api call
     try {
-        const { data } = await axios.post(`${Baseurl}/category`, {
-            title : category?.title
-        })
+      const { data } = await axios.post(
+        `${Baseurl}/category`,
+        {
+          title: category?.title,
+        },
+        config
+      );
+      return data;
     } catch (error) {
-        if(!error.response) {
-            throw error
-        }
-        return rejectWithValue(error?.response?.data)
+      if (!error.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
     }
-    }
-)
+  }
+);
 
 // Slices
 const categorySlices = createSlice({
-    name : "category",
-    initialState : {
-     category : "Node js"
-    },
-    extraReducers : (builder) =>{
-    builder.addCase(createCategoryAction.pending, (state, action) =>{
-        state.loading = true;
+  name: "category",
+  initialState: {
+    category: "Node js",
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createCategoryAction.pending, (state, action) => {
+      state.loading = true;
     });
 
-    builder.addCase(createCategoryAction.fulfilled, (state, action) =>{
-        state.loading = false;
-        state.category = action?.payload;
-        state.appError = undefined;
-        state.serverError = undefined;
-        // state.isCreated = true;
+    builder.addCase(createCategoryAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.category = action?.payload;
+      state.appError = undefined;
+      state.serverError = undefined;
+      // state.isCreated = true;
     });
 
-      builder.addCase(createCategoryAction.rejected, (state, action) =>{
-        state.loading = false;
-        state.category = action?.payload;
-           state.appError = action?.payload?.message;
-        state.serverError = action?.error;
-        // state.isCreated = true;
+    builder.addCase(createCategoryAction.rejected, (state, action) => {
+      state.loading = false;
+      state.category = action?.payload;
+      state.appError = action?.payload?.message;
+      state.serverError = action?.error;
+      // state.isCreated = true;
     });
+  },
+});
 
-    }
-})
-
-export default categorySlices.reducer
+export default categorySlices.reducer;
