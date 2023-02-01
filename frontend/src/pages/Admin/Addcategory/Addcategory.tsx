cf24f5a7c2454bbdde3eb709b9f1ffd57950cfa0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-01 22:23:32 +0100

    Working on Post category

--- frontend/src/pages/Admin/Addcategory/Addcategory.tsx
+++ frontend/src/pages/Admin/Addcategory/Addcategory.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Input } from "src/components/atoms";
 
 const Addcategory = () => {
+  const [title, setTitle] = useState<string>("");
   return (
-    <div>
-      Hello from Addcategory
-    </div>
-  )
-}
+    <Input
+      type="text"
+      placeholder="Create category"
+      onChange={(e: any) => setTitle(e.target.value)}
+      value={title}
+      className={"mt-2 focus:border-transparent focus:outline-transparent p-6 pl-8"}
+      label=""
+    />
+  );
+};
 
-export default Addcategory
+export default Addcategory;
