e4f8e6540a021276bab7db65f9c4c97d5bf0ac1a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-03 05:44:29 +0100

    waw

--- frontend/src/pages/Admin/Addcategory/Addcategory.js
+++ frontend/src/pages/Admin/Addcategory/Addcategory.js
@@ -4,8 +4,12 @@ import { toast } from "react-toastify";
 import { Input, Button } from "src/components/atoms";
 import { createCategoryAction } from "src/redux/Slices/Category/category";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-const Addcategory = () => {
+const Addcategory = (props) => {
+  const {id} = useParams();
+  console.log(id)
+  console.log({props})
     const [title, setTitle] = useState("");
   const dispatch = useDispatch();
 
