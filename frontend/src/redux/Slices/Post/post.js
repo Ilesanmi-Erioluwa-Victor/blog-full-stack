b79a0bd4c39be22b366cd7e8880a261a9fd14700
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-06 11:18:12 +0100

    Working on postSlices

--- frontend/src/redux/Slices/Post/post.js
+++ frontend/src/redux/Slices/Post/post.js
@@ -5,5 +5,23 @@ import { Baseurl } from "src/utils/Baseurl";
 // Create Post
 export const createPostAction = createAsyncThunk(
   "post/created",
-  async (post, { rejectWithValue, getState }) => {}
+  async (post, { rejectWithValue, getState }) => {
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+
+    try {
+      const { data } = await axios.post(`${Baseurl}/posts`, post, config);
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
 );
