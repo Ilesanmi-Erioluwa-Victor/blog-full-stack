f5318b2a1f739d5c91394a48387b7a3d922c41d4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-06 11:14:10 +0100

    Working on postSlices

--- frontend/src/redux/Slices/Post/post.js
+++ frontend/src/redux/Slices/Post/post.js
@@ -2,8 +2,8 @@ import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Baseurl } from "src/utils/Baseurl";
 
-
 // Create Post
-export const createPostAction = createAsyncThunk("post/created", async () => {
-    
-})
+export const createPostAction = createAsyncThunk(
+  "post/created",
+  async (post, { rejectWithValue, getState }) => {}
+);
