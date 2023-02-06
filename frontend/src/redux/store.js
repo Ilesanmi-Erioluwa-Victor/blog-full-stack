09d042b93fce5ab76c69455e105e2ec372efeec9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-06 11:26:54 +0100

    Added my post to my store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducers from "./Slices/Category/category"
 import userReducers from "./Slices/Users/user"
-
+import post from "./Slices/Post/post"
 export const store = configureStore({
   reducer: {
    users : userReducers,
-   category : categoryReducers
+   category : categoryReducers,
+   post
   },
 
 });
