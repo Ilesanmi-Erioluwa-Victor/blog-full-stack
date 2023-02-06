74552d4dc857cfd1c664d75002337a546e013361
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-06 11:48:08 +0100

    Almost done working on Create post

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Input } from 'src/components/atoms'
+
 
 const CreatePost = () => {
   return (
     <div className='block relative w-full padding'>
-      Hello from create post...
+      <div className='sec-flex '>
+        <h2 className='self-center text-2xl font-bold'>Create Post</h2>
+        <p className='self-center font-medium'>Create post, let's the world know who you are</p>
+          <form className='sec-flex gap-5'>
+           <Input type='text' 
+           className='p-6'
+            placeholder='Enter post title'
+            fieldsetClass='w-6/12 self-center mt-6'
+           />
+           <textarea placeholder='Enter post Content' className='w-6/12 self-center border rounded-lg p-3 h-40'/>
+          </form>
+      </div>
     </div>
   )
 }
