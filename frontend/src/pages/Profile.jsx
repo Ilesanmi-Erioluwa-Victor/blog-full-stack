a5e05c7cdc9313220170d643e36fa4581f508f4e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-05-23 17:28:56 +0100

    Working on Profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Icon } from 'src/utils';
 
 const Profile = () => {
-    const { id } = useParams();
-    console.log(id)
-  return <div>Hello {id}</div>;
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state?.users?.userAuth);
+  console.log(user);
+  const { id } = useParams();
+  return (
+    <section>
+      <main className='bg-gray-600'>
+        <div className='w-[70%] mx-auto bg-red-700 h-[30rem] rounded p-4'>
+          <Icon
+            src={user?.profilePhoto}
+            className='block max-width-[auto]'
+            alt={user?.firstName - user?.lastName}
+          />
+        </div>
+      </main>
+    </section>
+  );
 };
 
 export default Profile;
