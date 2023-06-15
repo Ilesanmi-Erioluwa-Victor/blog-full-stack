aa4cf8b375575395b5faec64e439a17e698fa7c2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-15 21:17:25 +0100

    Refactoring codebase

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ export { default as Home } from "./Home"
 export { default as Error } from "./Error"
 export { default as Signup } from "./auth/Signup"
 export { default as Login } from "./auth/Login"
-export { default as CreatePost } from "./CreatePost"
-export { default as PostList } from "./PostList"
+export { default as CreatePost } from "./Post/CreatePost"
+export { default as PostList } from "./Post/PostList"
 export { default as Profile } from "./Profile"
-export { default as Post } from './Post';
+export { default as Post } from './Post/Post';
